b53f7e55883223ba31db55a1651a12a6690a70ad
Brady Williams <williams.cole29@yahoo.com>
2021-10-15 14:14:46 -0500

    Finished initial file setup

---
+++ src/DriftingApart.js
@@ -0,0 +1,12 @@
+
+function DriftingApart() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Drifting Apart</h1>
+      </header>
+    </div>
+  );
+}
+
+export default DriftingApart;
