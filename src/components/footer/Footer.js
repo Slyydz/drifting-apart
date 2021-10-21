8c7a6f0d6514fa2bdb5eef1031bec0821f53dfd1
Brady Williams <williams.cole29@yahoo.com>
2021-10-21 09:34:43 -0500

    yuuup

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -12,21 +12,24 @@ export const Footer = () => {
     return (
         <footer className="footer">
             <div className="contain-footer">
-            <ul className="nav-footer">
-                <li className="nav__item">
-                    <Link className="nav__link" to="/vehicles">My Vehicles</Link>
-                </li>
-                <li className="nav__item">
-                    <Link className="nav__link" to="/upcoming">Upcoming Events</Link>
-                </li>
-                <li className="nav__item">
-                    <Link className="nav__link" to="/past">Past Events</Link>
-                </li>
-                <li className="nav__item">
-                    <Link className="nav__link" to="/gallery">Photo Gallery</Link>
-                </li>
-            </ul>
-            <p>&copy; Nashville Software School</p>
+                <ul className="nav-footer">
+                    <li className="nav__item">
+                        <Link className="nav__link" to="/">Home</Link>
+                    </li>
+                    <li className="nav__item">
+                        <Link className="nav__link" to="/vehicles">My Vehicles</Link>
+                    </li>
+                    <li className="nav__item">
+                        <Link className="nav__link" to="/upcoming">Upcoming Events</Link>
+                    </li>
+                    <li className="nav__item">
+                        <Link className="nav__link" to="/past">Past Events</Link>
+                    </li>
+                    <li className="nav__item">
+                        <Link className="nav__link" to="/gallery">Photo Gallery</Link>
+                    </li>
+                </ul>
+                <p>&copy; Nashville Software School</p>
             </div>
         </footer>
     )
--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -7,12 +7,15 @@ import "../nav/NavBar.css"
 
 export const NavBar = () => {
 
-const history = useHistory();  
+  const history = useHistory();
 
   return (
     <nav className="nav__flex">
       <Link to="/"> <img className="logo" src={logo} alt="Drifting Apart Logo" /></Link>
       <ul className="nav">
+        <li className="nav__item">
+          <Link className="nav__link" to="/">Home</Link>
+        </li>
         <li className="nav__item">
           <Link className="nav__link" to="/vehicles">My Vehicles</Link>
         </li>
@@ -27,9 +30,9 @@ const history = useHistory();
         </li>
       </ul>
       <button className="nav__button" onClick={() => {
-            sessionStorage.removeItem("drifting_user");
-            history.push("/login")
-        }}>Logout</button>
+        sessionStorage.removeItem("drifting_user");
+        history.push("/login")
+      }}>Logout</button>
     </nav>
   )
 }
