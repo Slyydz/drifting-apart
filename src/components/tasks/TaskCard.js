bf6f19f98fe33b70afdaeca7e67552acaeeeed32
Brady Williams <williams.cole29@yahoo.com>
2021-10-22 15:18:57 -0500

    Tasks can now be marked completed

--- src/components/tasks/TaskCard.js
+++ src/components/tasks/TaskCard.js
@@ -1,19 +1,43 @@
 import React from "react";
 import { useHistory } from "react-router";
+import { update } from "./TaskManager";
+import { useState } from "react";
 
-export const TaskCard = ({task, handleDelete}) => {
+export const TaskCard = ({ task, handleDelete, getTasks }) => {
 
-        const history = useHistory();
+  const history = useHistory();
 
+  const [isTrue, setTrue] = useState(true);
 
-        return (
-            <div className="card">
-              <div className="task-card">
-                <h2>{task.taskName}</h2>
-                <h4>{task.taskDesc}</h4>
-                <button className="tasks-delete" onClick={() => handleDelete(task.id)}>Delete</button>
-                <button className="tasks-edit" onClick={() => history.push(`/upcoming/tasks/${task.eventId}/edit/${task.id}`)}>Edit</button>
-              </div>
-            </div>
-          )
+  const changeCompleted = () => {
+
+    const completeTask = {
+      id: task.id,
+      taskName: task.taskName,
+      taskDesc: task.taskDesc,
+      eventId: task.eventId,
+      userId: parseInt(sessionStorage.getItem("drifting_user")),
+      isCompleted: true
     }
+
+    update(completeTask).then(() => {
+      getTasks();
+      setTrue(false);
+    })
+  }
+
+
+  return (
+    <div className="card-task">
+      <div className="task-card">
+        <h2>{task.taskName}</h2>
+        <h4>{task.taskDesc}</h4>
+        {task.isCompleted ? null : <button className="tasks-delete" onClick={() => handleDelete(task.id)}>Delete</button>}
+        {task.isCompleted ? null : <button className="tasks-edit" onClick={() => history.push(`/upcoming/tasks/${task.eventId}/edit/${task.id}`)}>Edit</button>}
+      </div>
+      <div className="checkbox">
+        {task.isCompleted ? null : <input type="checkbox" name="completed" onClick={() => changeCompleted()} />}
+      </div>
+    </div>
+  )
+}
