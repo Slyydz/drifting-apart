1e021e4bf38d5fcbd0a6176beaab2ba64e647b4f
Brady Williams <williams.cole29@yahoo.com>
2021-10-25 15:52:41 -0500

    Fixed improper placement of headers

--- src/components/vehicles/Vehicles.js
+++ src/components/vehicles/Vehicles.js
@@ -28,9 +28,9 @@ export const Vehicles = () => {
   return (
     <>
       <div className="main-content">
+        <h1 className="vehicles-title">My Vehicles:</h1>
+        <button className="vehicles-add" onClick={() => history.push("/vehicles/add")}>Add A Vehicle +</button>
         <div className="scroll-box">
-          <h1 className="vehicles-title">My Vehicles:</h1>
-          <button className="vehicles-add" onClick={() => history.push("/vehicles/add")}>Add A Vehicle +</button>
           <div className="vehicles-list">
             {vehicles.length !== 0 ? vehicles.map(vehicle => <VehicleCard key={vehicle.id} vehicle={vehicle} handleDelete={handleDeleteVehicle} />) : <h2>No Vehicles Yet</h2>}
           </div>
