29bb2815764b482d599537fa106848319cc305d6
Brady Williams <williams.cole29@yahoo.com>
2021-10-27 15:56:17 -0500

    Users name now appear on photo gallery next to their post

--- src/components/gallery/PhotoGalleryCard.js
+++ src/components/gallery/PhotoGalleryCard.js
@@ -1,16 +1,26 @@
-import React from "react";
-import { Image } from 'cloudinary-react'
+import React, { useEffect, useState } from "react";
+import { Image } from 'cloudinary-react';
+import { getUserById } from "../upcoming/UpcomingEventManager";
 
 
 export const PhotoCard = ({ photo }) => {
 
+    const [userState, setUser] = useState({});
+
+    const getUserDom = () => {
+        getUserById(photo.userId).then((res) => setUser(res))
+    }
+
+    useEffect(() => {
+        getUserDom();
+    }, [])
 
 
     return (
         <div className="card-task">
             <div className="task-card">
                 <Image style={{ width: 250, height: 200 }} cloudName="dkchtgrs0" publicId={photo.photo} />
-                <h4>{photo.userId}</h4>
+                <h4>Posted By:</h4><h4>{userState.name}</h4>
             </div>
         </div>
     )
